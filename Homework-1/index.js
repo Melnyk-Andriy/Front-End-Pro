ed0ad26c033d5e157918d5e01b2bb8f34436db3c
Melnyk-Andriy <61824627+Melnyk-Andriy@users.noreply.github.com>
2020-04-22 19:21:17 +0300

    Create index.js

---
+++ Homework-1/index.js
@@ -0,0 +1,8 @@
+console.log("1) Method log");
+console.info("2) Method info");
+console.warn("3) Method warn");
+console.error("4) Method error");
+console.group("5) Method group");
+  console.log("some text");
+  console.log("some text");
+console.groupEnd();
