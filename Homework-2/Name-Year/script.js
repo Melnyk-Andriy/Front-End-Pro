bc9a39fc0413a1dcda4bbdb45112d28c655c9bbb
Melnyk-Andriy <61824627+Melnyk-Andriy@users.noreply.github.com>
2020-04-22 19:26:05 +0300

    Create script.js

---
+++ Homework-2/Name-Year/script.js
@@ -0,0 +1,8 @@
+let name = prompt('Как тебя зовут ?', 'Андрей');
+let year = prompt('Какого ты года рождения ?', 2000);
+
+const thisYear = 2020;
+
+let age = thisYear - year;
+
+alert(name + ' ' + age);
