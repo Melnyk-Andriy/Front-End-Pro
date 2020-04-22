a8cdf7a1fd5ac94522917f15fc5569801ffe6d29
Melnyk-Andriy <61824627+Melnyk-Andriy@users.noreply.github.com>
2020-04-22 19:29:28 +0300

    Create script.js

---
+++ Homework-2/Numbers/script.js
@@ -0,0 +1,10 @@
+let num_1 = prompt('Введите первое число:');
+let num_2 = prompt('Введите второе число:');
+let num_3 = prompt('Введите третие число:');
+
+let a = Number(num_1);
+let b = Number(num_2);
+let c = Number(num_3);
+
+console.log("Cума всех чисел -", a + b + c);
+console.log("среднее арифметическое всех чисел -", (a + b + c) / 3);
